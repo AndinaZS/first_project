bc3dcfbf817235559bd6a84c38b974e83fc760e8
AndinaZS <alexa.markova@outlook.com>
2021-12-05 20:34:51 +0300

    change functions

--- main.py
+++ main.py
@@ -16,17 +16,23 @@ def get_url_foto(ownerID, num=5):
     'count': num,
     'access_token': token_vk,
     'v':'5.131'}
-    res = requests.get(url, params=params).json()['response']['items']
+    response = requests.get(url, params=params).json()['response']['items']
+    create_json(response)
+    return
+
+def create_json(response):
     with open("data_file.json", "w") as write_file:
-        for elem in res:
-            if dct.get(str(elem['likes']['count']), 0):
+        set_filenames = set()
+        for elem in response:
+            if str(elem['likes']['count'])in set_filenames:
                 date = datetime.utcfromtimestamp(elem['date']).strftime('%d-%m-%Y')
                 file_name = str(elem['likes']['user_likes']) + str(date)
             else:
                 file_name = str(elem['likes']['count'])
-            dct[file_name] = elem['sizes'][-1]['url']
+            upload_foto(url=elem['sizes'][-1]['url'],file_name=file_name)
             json.dump({'file_name': f'{file_name}.jpg', 'size': elem['sizes'][-1]['type']}, write_file)
-    return dct
+            set_filenames.add(file_name)
+    return
 
 def create_folder(folder_name):
     upload_url = "https://cloud-api.yandex.net/v1/disk/resources"
@@ -34,16 +40,14 @@ def create_folder(folder_name):
     params = {'path': folder_name}
     response = requests.put(upload_url, headers=headers, params=params)
 
-def upload_foto(dct_foto):
+def upload_foto(url, file_name):
     upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
     headers = {'Content-Type': 'application/json',
             'Authorization': f'OAuth {token_ya}'}
-    for key, value in dct_foto.items():
-        params = {"url": value, 'path':f'1565606/{key}'}
-        response = requests.post(upload_url, headers=headers, params=params)
-        pprint(response.status_code)
+    params = {"url": url, 'path':f'1565606/{file_name}'}
+    response = requests.post(upload_url, headers=headers, params=params)
+    pprint(response.status_code)
 
 if __name__ == '__main__':
     dct = get_url_foto('1565606', 10)
     create_folder('1565606')
-    upload_foto(dct)
